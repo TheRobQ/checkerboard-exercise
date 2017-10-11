70cae64455434f7911713680e9d4699937c5afa4
Robert Quan <rob.quan@gmail.com>
2017-10-11 16:20:22 -0600

    Random Colors Work

--- script.js
+++ script.js
@@ -8,10 +8,11 @@ for(var i = 0; i < 81; i++){
 div.style.float ='left';
 div.style.paddingBottom = '11.1%';
 div.style.width = '11.1%';
+//div.style.backgroundColor = 'red'
 // if(i % 2 === 0){
 //        div.style.backgroundColor = 'black';
 //        }
 // else {div.style.backgroundColor = 'red'};
-div.style.backgroundColor = rgb(234, 23, 35)
+div.style.backgroundColor = 'rgb(' + (Math.floor(Math.random() * 255)) + ',' + (Math.floor(Math.random() * 255)) + ',' + (Math.floor(Math.random() * 255)) + ')';
 body.appendChild(div)
 }
