2600c9ffd79422bee0ddd5f06e8a3a54ed1984de
Robert Quan <rob.quan@gmail.com>
2017-10-11 15:01:03 -0600

    Black and Red checkboard works

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 // Your JS goes here
+
+for(var i = 0; i < 81; i++){
+  var body = document.getElementsByTagName('body')[0];
+  var div = document.createElement('div');
+
+div.style.float ='left';
+div.style.paddingBottom = '11.1%';
+div.style.width = '11.1%';
+if(i % 2 === 0){
+       div.style.backgroundColor = 'black';
+       }
+else {div.style.backgroundColor = 'red'};
+body.appendChild(div)
+}
