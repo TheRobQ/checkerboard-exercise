29a20dcc93ffe12e45cfbe247eff35b03e844620
Robert Quan <rob.quan@gmail.com>
2017-10-12 15:23:01 -0600

    Flashing Works

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
-// document.addEventListener("DOMContentLoaded", function){
-// Your JS goes here
+document.addEventListener("DOMContentLoaded", function(){
 
-for(var i = 0; i < 81; i++){
-  var body = document.getElementsByTagName('body')[0];
-  var div = document.createElement('div');
-  body.appendChild(div);
-div.style.float ='left';
-div.style.paddingBottom = '11.1%';
-div.style.width = '11.1%';
-div.style.backgroundColor = 'rgb('+ (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) +')'
-function flashBox(){
-  for(var i = 0; i < 81; i++){
-  div.style.backgroundColor = 'rgb('+ (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) +')';
-      }
-    }   
+  for (var i = 0; i < 81; i++) {
+    var body = document.getElementsByTagName('body')[0]
+    var div = document.createElement('div');
+    let red = (Math.floor(Math.random() * 256)).toString();
+    let green = (Math.floor(Math.random() * 256)).toString();
+    let blue = (Math.floor(Math.random() * 256)).toString();
+    div.style.float = 'left';
+    div.style.width = '11.1%';
+    div.style.paddingBottom = '11.1%';
+    div.style.backgroundColor = "rgb(" + red + ", " + green + ", " + blue + ")"
+    body.appendChild(div);
+  }
+  function flashBox() {
+    for (var i = 0; i < 81; i++) {
+      let red = (Math.floor(Math.random() * 256)).toString();
+      let green = (Math.floor(Math.random() * 256)).toString();
+      let blue = (Math.floor(Math.random() * 256)).toString();
+      body.children[i].style.backgroundColor = "rgb(" + red + ", " + green + ", " + blue + ")"
+    }
+  }
 setInterval(flashBox, 2000);
-}
+
+})
